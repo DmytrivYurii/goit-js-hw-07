ac677cf4d19ab50ccf0ff707e10c1c2d196b84a8
DmytrivYurii <73756245+DmytrivYurii@users.noreply.github.com>
2023-11-15 10:14:10 +0200

    1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -28,7 +28,7 @@ function renderGallery() {
 
     const image = document.createElement("img");
     image.classList.add("gallery__image");
-    image.src = item.smallImage;
+    image.src = item.smallйImage;
     image.alt = item.description;
     image.dataset.source = item.largeImage;
 
